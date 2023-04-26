66a9402c7337efa411a0d2cd0e65705ffe61f777
nuhgnoesoj <126743224+nuhgnoesoj@users.noreply.github.com>
2023-04-26 15:56:07 +0900

    chap04 Score App 등록 & 전체조회 & 삭제 & 상세조회

---
+++ src/test/java/com/spring/mvc/chap04/repository/StudentRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.spring.mvc.chap04.repository;
+
+import com.spring.mvc.chap04.dto.ScoreRequestDTO;
+import com.spring.mvc.chap04.entity.Score;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    StudentRepository repository;
+
+
+    @Test
+    @DisplayName("학생의 이름,점수,평균,총점,등급를 DB student table에 잘 삽입해야 한다.")
+    void saveTest() {
+        ScoreRequestDTO dto = new ScoreRequestDTO();
+        dto.setName("홍길동");
+        dto.setKor(50);
+        dto.setEng(70);
+        dto.setMath(80);
+        Score s = new Score(dto);
+
+        repository.save(s);
+    }
+}
